b75940248152849020ffe48227d2892105a83e0e
Hayden Sexton <Hayden_Sexton@insideranken.org>
2025-09-17 16:01:55 -0500

    Commit CH3 Final Proj and CH2 Lab fixes
    Final proj bootstrap updated with libman to latest and title changed for SEO, CH2 labs error messages fixed

--- Labs/CH02/P2/P2-1 Price Quotation App/Models/PriceQuotationModel.cs
+++ Labs/CH02/P2/P2-1 Price Quotation App/Models/PriceQuotationModel.cs
@@ -6,23 +6,23 @@ namespace P2_1_Price_Quotation_App.Models
     {
         [Required(ErrorMessage = "Please enter a subtotal.")]
         [Range(0.0000001, double.MaxValue, ErrorMessage = "Please enter a value greater than 0.")]
-        public decimal Subtotal { get; set; }
+        public decimal? Subtotal { get; set; }
 
         [Required(ErrorMessage = "Please enter a discount percent.")]
         [Range(1, 100, ErrorMessage = "Please enter percent between 1 and 100.")]
-        public decimal DiscountPercent { get; set; }
+        public decimal? DiscountPercent { get; set; }
 
-        public decimal CalculateTotal()
+        public decimal? CalculateTotal()
         {
-            decimal discountAmount = Subtotal * (DiscountPercent / 100);
-            decimal total = Subtotal - discountAmount;
+            decimal? discountAmount = Subtotal * (DiscountPercent / 100);
+            decimal? total = Subtotal - discountAmount;
 
             return total;
         }
 
-        public decimal CalculateDiscountAmount()
+        public decimal? CalculateDiscountAmount()
         {
-            decimal discountAmount = Subtotal * (DiscountPercent / 100);
+            decimal? discountAmount = Subtotal * (DiscountPercent / 100);
 
             return discountAmount;
         }
