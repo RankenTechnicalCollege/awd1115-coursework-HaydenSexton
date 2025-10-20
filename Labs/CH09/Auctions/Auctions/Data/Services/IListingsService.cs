a0e4d9996393f2f5cef75c1e5c7c78fa2e4ae34a
Hayden Sexton <Hayden_Sexton@insideranken.org>
2025-10-19 23:12:29 -0500

    Commit CH9 Lab

---
+++ Labs/CH09/Auctions/Auctions/Data/Services/IListingsService.cs
@@ -0,0 +1,13 @@
+﻿using Auctions.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auctions.Data.Services
+{
+    public interface IListingsService
+    {
+        IQueryable<Listing> GetAll();
+        Task Add(Listing listing);
+        Task<Listing> GetById(int? id);
+        Task SaveChanges();
+    }
+}
