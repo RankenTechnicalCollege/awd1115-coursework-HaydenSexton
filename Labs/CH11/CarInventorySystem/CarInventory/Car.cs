a9551be4689c62bdd453f163e013be38a79874d8
Hayden Sexton <Hayden_Sexton@insideranken.org>
2025-10-29 04:57:58 -0500

    Commit CH11 Lab MinimalAPI

---
+++ Labs/CH11/CarInventorySystem/CarInventory/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace CarInventory
+{
+    public class Car
+    {
+        // primary key
+        public int Id { get; set; }
+
+        // car properties
+        public string Make {  get; set; }
+        public string Model { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public string? Secret { get; set; }
+    }
+}
---
+++ Labs/CH11/CarInventorySystem/CarInventory/CarDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace CarInventory
+{
+    public class CarDTO
+    {
+        // primary key
+        public int Id { get; set; }
+
+        // car properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public CarDTO() { }
+
+        public CarDTO(Car car)
+        {
+            Id = car.Id;
+            Make = car.Make;
+            Model = car.Model;
+            IsAvailable = car.IsAvailable;
+        }
+    }
+}
